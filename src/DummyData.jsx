20c5d989a5e406cc5118a60e17bfefc533dfc3a1
Anand6262 <pyexpert62@gmail.com>
2025-03-09 20:35:14 +0530

    data

---
+++ src/DummyData.jsx
@@ -0,0 +1,14 @@
+export const data = [
+    {
+      "id": 1,
+      "firstName": "John",
+      "lastName": "Doe",
+      "age": 28
+    },
+    {
+      "id": 2,
+      "firstName": "Jane",
+      "lastName": "Smith",
+      "age": 34
+    }
+  ]
